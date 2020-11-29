4bb979a70252b79a28227193983172595d1a1020
Marcin Gol <golmarcin@gmail.com>
2020-11-29 21:33:32 +0100

    extract side menu as separate component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
 
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { Link } from "gatsby";
-import { StaticQuery, graphql } from "gatsby";
-import { HelmetDatoCms } from "gatsby-source-datocms";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
-import "../styles/index.sass";
+import '../styles/index.sass';
+import MainNavigation from './navigation/main-navigation';
 
 const TemplateWrapper = ({ children }) => {
   const [showMenu, setShowMenu] = useState(false);
@@ -43,58 +44,28 @@ const TemplateWrapper = ({ children }) => {
           }
         }
       `}
-      render={data => (
-        <div className={`container ${showMenu ? "is-open" : ""}`}>
+      render={(data) => (
+        <div className={`container ${showMenu ? 'is-open' : ''}`}>
           <HelmetDatoCms
             favicon={data.datoCmsSite.faviconMetaTags}
             seo={data.datoCmsHome.seoMetaTags}
           />
-          <div className="container__sidebar">
-            <div className="sidebar">
-              <h6 className="sidebar__title">
-                <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-              </h6>
-              <div
-                className="sidebar__intro"
-                dangerouslySetInnerHTML={{
-                  __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
-                }}
-              />
-              <ul className="sidebar__menu">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-              <p className="sidebar__social">
-                {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-                  <a
-                    key={profile.profileType}
-                    href={profile.url}
-                    target="blank"
-                    className={`social social--${profile.profileType.toLowerCase()}`}
-                  >
-                    {" "}
-                  </a>
-                ))}
-              </p>
-              <div className="sidebar__copyright">
-                {data.datoCmsHome.copyright}
-              </div>
-            </div>
-          </div>
+          <MainNavigation
+            datoCmsSite={data.datoCmsSite}
+            allDatoCmsSocialProfile={data.allDatoCmsSocialProfile}
+            datoCmsHome={data.datoCmsHome}
+          />
           <div className="container__body">
             <div className="container__mobile-header">
               <div className="mobile-header">
                 <div className="mobile-header__menu">
                   <button
-                    onClick={e => {
+                    id="mobile-header__hamburger-menu"
+                    onClick={(e) => {
                       e.preventDefault();
                       setShowMenu(!showMenu);
                     }}
+                    aria-label="Toggle menu"
                   />
                 </div>
                 <div className="mobile-header__logo">
@@ -111,7 +82,7 @@ const TemplateWrapper = ({ children }) => {
 };
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.object
+  children: PropTypes.object,
 };
 
 export default TemplateWrapper;
---
+++ src/components/navigation/main-navigation.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+
+function MainNavigation({ datoCmsSite, datoCmsHome, allDatoCmsSocialProfile }) {
+  return (
+    <div className="container__sidebar">
+      <div className="sidebar">
+        <h6 className="sidebar__title">
+          <Link to="/">{datoCmsSite.globalSeo.siteName}</Link>
+        </h6>
+        <div
+          className="sidebar__intro"
+          dangerouslySetInnerHTML={{
+            __html: datoCmsHome.introTextNode.childMarkdownRemark.html,
+          }}
+        />
+        <ul className="sidebar__menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <p className="sidebar__social">
+          {allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+            <a
+              key={profile.profileType}
+              href={profile.url}
+              target="blank"
+              className={`social social--${profile.profileType.toLowerCase()}`}
+            >
+              {' '}
+            </a>
+          ))}
+        </p>
+        <div className="sidebar__copyright">{datoCmsHome.copyright}</div>
+      </div>
+    </div>
+  );
+}
+
+MainNavigation.propTypes = {
+  datoCmsSite: PropTypes.object,
+  allDatoCmsSocialProfile: PropTypes.object,
+  datoCmsHome: PropTypes.object,
+};
+
+export default MainNavigation;
