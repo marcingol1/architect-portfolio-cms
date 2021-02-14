44ffbdab132924f2a239f986c6aaded85262a1d2
Marcin Gol <golmarcin@gmail.com>
2021-02-14 18:37:14 +0100

    remove console.log

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 
 function Footer({ allDatoCmsSocialProfile, datoCmsHome, datoCmsAboutPage }) {
-  console.log({ datoCmsAboutPage });
   return (
     <footer className="container__footer">
       <p className="footer__social">
