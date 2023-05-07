ff8eb8932eb13db8338559761b6dfea3bcd7c5de
Marcin Gol <golmarcin@gmail.com>
2023-05-07 19:41:22 +0200

    feat: change title

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Architektura Anna Gol
+        Architekt Anna Gol
       </h1>
       <Suspense fallback={<TablePlaceholder />}>
         {/* @ts-expect-error Async Server Component */}
